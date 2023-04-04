94e3ae08a21ab32dd2d932c2d288ad3a773807a2
Griffins Gichure <g.gichure@outlook.com>
2023-04-04 20:39:45 +0300

    LocalStoragePuzzlesApi fucntions

--- packages/local_storage_puzzles_api/lib/src/local_storage_puzzles_api.dart
+++ packages/local_storage_puzzles_api/lib/src/local_storage_puzzles_api.dart
@@ -1,7 +1,62 @@
+import 'package:local_storage_puzzles_api/src/database/db.dart';
+import 'package:local_storage_puzzles_api/src/database/puzzles_dao.dart';
+
 /// {@template local_storage_puzzles_api}
 /// A Flutter implementation of the Puzzles  that uses local storage.
 /// {@endtemplate}
+///
+/// {@macro puzzles_dao}
 class LocalStoragePuzzlesApi {
   /// {@macro local_storage_puzzles_api}
-  const LocalStoragePuzzlesApi();
+  const LocalStoragePuzzlesApi({
+    required PuzzlesDao puzzlesDao,
+  }) : _puzzlesDao = puzzlesDao;
+  final PuzzlesDao _puzzlesDao;
+
+  /// {@macro puzzles_getPuzzles}
+  Stream<List<PuzzlesModelTableData>> getPuzzles() async* {
+    yield* _puzzlesDao.getPuzzles();
+  }
+
+  /// {@macro puzzles_savePuzzle}
+  Future<void> savePuzzle(
+    PuzzlesModelTableData? puzzlesModelTableData,
+  ) async {
+    return _puzzlesDao.savePuzzle(puzzlesModelTableData);
+  }
+
+  /// {@macro puzzles_savePuzzleItems}
+  Future<void> savePuzzleItems(
+    PuzzlesItemsModelTableData? puzzlesItemsModelTableData,
+  ) async {
+    return _puzzlesDao.savePuzzleItems(puzzlesItemsModelTableData);
+  }
+
+  /// {@macro puzzles_deletePuzzleItems}
+  Future<void> deletePuzzleItem(
+    int puzzleItemId,
+  ) async {
+    return _puzzlesDao.deletePuzzleItem(puzzleItemId);
+  }
+
+  /// {@macro puzzles_deleteAllPuzzles}
+  Future<void> deleteAllPuzzles(
+    int puzzleItemId,
+  ) async {
+    return _puzzlesDao.deleteAllPuzzles();
+  }
+
+  /// {@macro puzzles_deletePuzzle}
+  Future<void> deletePuzzle(
+    int puzzleId,
+  ) async {
+    return _puzzlesDao.deletePuzzle(puzzleId);
+  }
+
+  /// {@macro puzzles_getPuzzlesItems}
+  Stream<List<PuzzlesItemsModelTableData>> getPuzzlesItems(
+    int puzzleId,
+  ) async* {
+    yield* _puzzlesDao.getPuzzlesItems(puzzleId);
+  }
 }
