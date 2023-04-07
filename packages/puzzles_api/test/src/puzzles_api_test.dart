94091205a3a74f4671f5aadb8189e02ecbbb904f
Griffins Gichure <g.gichure@outlook.com>
2023-04-07 17:48:12 +0300

    test puzzles api

--- packages/puzzles_api/test/src/puzzles_api_test.dart
+++ packages/puzzles_api/test/src/puzzles_api_test.dart
@@ -2,10 +2,19 @@
 import 'package:puzzles_api/puzzles_api.dart';
 import 'package:test/test.dart';
 
+class TestTodosApi extends PuzzlesApi {
+  TestTodosApi() : super();
+
+  @override
+  dynamic noSuchMethod(Invocation invocation) {
+    return super.noSuchMethod(invocation);
+  }
+}
+
 void main() {
   group('PuzzlesApi', () {
-    test('can be instantiated', () {
-      expect(PuzzlesApi(), isNotNull);
+    test('can be constructed', () {
+      expect(TestTodosApi.new, returnsNormally);
     });
   });
 }
