0258b64d3b81b2a01a4ad96bd16109f14ab942fb
Griffins Gichure <g.gichure@outlook.com>
2023-04-08 10:54:57 +0300

    fix json keys, test puzzle_item_dao

--- packages/local_storage_puzzles_api/test/src/database/daos/puzzles_item_dao_test.dart
+++ packages/local_storage_puzzles_api/test/src/database/daos/puzzles_item_dao_test.dart
@@ -1,25 +1,10 @@
+import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:local_storage_puzzles_api/src/database/daos/puzzle_items_dao.dart';
-import 'package:mockito/mockito.dart';
-import 'package:drift/drift.dart';
 import 'package:local_storage_puzzles_api/src/database/db.dart';
-import 'package:local_storage_puzzles_api/src/database/tables.dart';
+import 'package:mockito/mockito.dart';
 import 'package:puzzles_api/puzzles_api.dart';
-
-class MockMyDatabase extends Mock implements MyDatabase {
-  final QueryExecutor queryExecutor;
-
-  MockMyDatabase(this.queryExecutor);
-
-  // @override
-  // PuzzlesItemTable get puzzlesItemTable => MockPuzzlesItemTable();
-
-  @override
-  Future<void> close() async {}
-
-  // Implement any other methods or properties of MyDatabase that you need to mock
-}
 
 class MockPuzzlesItemDao extends Mock implements PuzzlesItemDao {
   @override
@@ -32,15 +17,18 @@ class MockPuzzlesItemDao extends Mock implements PuzzlesItemDao {
   Future<void> deletePuzzleItem(PuzzleItem puzzleItem) => Future.value();
 
   @override
-  Future<List<PuzzleItem>>? getAllPuzzlesItems(int id) => Future.value([]);
+  Future<List<PuzzleItem>>? getAllPuzzlesItems(int id) =>
+      Future.value(puzzleItems);
 
   @override
-  Stream<List<PuzzleItem>> streamAllPuzzlesItems(int id) => Stream.value([]);
+  Stream<List<PuzzleItem>> streamAllPuzzlesItems(int id) =>
+      Stream.value(puzzleItems);
 
   @override
-  Future<PuzzleItem?>? getPuzzleItemById(int id) => Future.value();
+  Future<PuzzleItem?>? getPuzzleItemById(int id) => Future.value(puzzleItem);
 }
 
+final puzzleItems = [puzzleItem];
 final puzzleItem = PuzzleItem(
   id: 1,
   puzzleId: 1,
@@ -49,80 +37,80 @@ final puzzleItem = PuzzleItem(
   completedAt: DateTime.november.toString(),
   choices: const ['Item 1', 'item 2'],
 );
-// void main() {
-//   TestWidgetsFlutterBinding.ensureInitialized();
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
 
-//   late MockMyDatabase mockDatabase;
-//   late PuzzlesItemDao puzzlesItemDao;
-//   group('PuzzlesItemDao tests', () {
-//     setUp(() {
-//       final inMemory = DatabaseConnection(NativeDatabase.memory());
-//       // Initialize the database with the temporary file
-//       mockDatabase = MockMyDatabase(inMemory);
+  late MyDatabase mockDatabase;
+  late PuzzlesItemDao puzzlesItemDao;
+  group('PuzzlesItemDao tests', () {
+    setUp(() {
+      final inMemory = DatabaseConnection(NativeDatabase.memory());
+      // Initialize the database with the temporary file
+      mockDatabase = MyDatabase(inMemory);
 
-//       puzzlesItemDao = PuzzlesItemDao(mockDatabase);
-//     });
+      puzzlesItemDao = PuzzlesItemDao(mockDatabase);
+    });
+    // Close the database after each test
+    tearDown(() async {
+      await mockDatabase.close();
+    });
+    test('Test insertPuzzleItem', () async {
+      await puzzlesItemDao.insertPuzzleItem(puzzleItem);
+      // ignore: inference_failure_on_instance_creation
 
-//     test('Test insertPuzzleItem', () async {
-//       await puzzlesItemDao.insertPuzzleItem(puzzleItem);
+      // Verify that the insertPuzzleItem method is called with the correct
+      // argument
+      final result = await puzzlesItemDao.getPuzzleItemById(1);
 
-//       // Verify that the insertPuzzleItem method is called with the correct argument
-//       verify(
-//         mockDatabase.puzzlesItemTable.insertOnConflictUpdate(
-//           PuzzlesItemTableData.fromJson(puzzleItem.toJson()),
-//         ),
-//       ).called(1);
-//     });
+      expect(puzzleItem, equals(result));
+    });
 
-//     test('Test updatePuzzleItem', () async {
-//       await puzzlesItemDao.updatePuzzleItem(puzzleItem);
+    test('Test updatePuzzleItem', () async {
+      await puzzlesItemDao.insertPuzzleItem(puzzleItem);
+      await puzzlesItemDao
+          .updatePuzzleItem(puzzleItem.copyWith(createdAt: '12'));
 
-//       // Verify that the updatePuzzleItem method is called with the correct argument
-//       verify(mockDatabase.puzzlesItemTable
-//               .replaceOne(PuzzlesItemTableData.fromJson(puzzleItem.toJson())))
-//           .called(1);
-//     });
+      // Verify that the updatePuzzleItem method is called with the correct
+      // argument
+      final result = await puzzlesItemDao.getPuzzleItemById(1);
 
-//     test('Test deletePuzzleItem', () async {
-//       await puzzlesItemDao.deletePuzzleItem(puzzleItem);
+      expect(puzzleItem.copyWith(createdAt: '12'), equals(result));
+    });
 
-//       // Verify that the deletePuzzleItem method is called with the correct
-//       // argument
-//       verify(
-//         mockDatabase.puzzlesItemTable
-//             .delete()
-//             .delete(PuzzlesItemTableData.fromJson(puzzleItem.toJson())),
-//       ).called(1);
-//     });
+    test('Test deletePuzzleItem', () async {
+      await puzzlesItemDao.insertPuzzleItem(puzzleItem);
+      await puzzlesItemDao.deletePuzzleItem(puzzleItem);
 
-//     test('Test getAllPuzzlesItems', () async {
-//       final id = 1;
-//       final puzzleItems = [puzzleItem];
-//       when(puzzlesItemDao.getAllPuzzlesItems(id))
-//           .thenAnswer((_) => Future.value(puzzleItems));
+      // Verify that the updatePuzzleItem method is called with the correct
+      // argument
+      final result = await puzzlesItemDao.getPuzzleItemById(1);
 
-//       final result = await puzzlesItemDao.getAllPuzzlesItems(id);
+      expect(result, null);
+    });
 
-//       // Verify that the getAllPuzzlesItems method is called with the correct argument
-//       verify(mockDatabase.puzzlesItemTable.select().get()).called(1);
+    test('Test getAllPuzzlesItems', () async {
+      const id = 1;
 
-//       // Verify that the result matches the expected puzzleItems
-//       expect(result, puzzleItems);
-//     });
+      await puzzlesItemDao.insertPuzzleItem(puzzleItem);
 
-//     test('Test streamAllPuzzlesItems', () async {
-//       const id = 1;
-//       final puzzleItems = [puzzleItem];
-//       when(puzzlesItemDao.streamAllPuzzlesItems(id))
-//           .thenAnswer((_) => Stream.value(puzzleItems));
+      final result = await puzzlesItemDao.getAllPuzzlesItems(id);
 
-//       final result = puzzlesItemDao.streamAllPuzzlesItems(id);
+      // Verify that the result matches the expected puzzleItems
+      expect(result, puzzleItems);
+    });
 
-//       // Verify that the streamAllPuzzlesItems method is called with the correct argument
-//       verify(mockDatabase.puzzlesItemTable.select().watch()).called(1);
+    test('Test streamAllPuzzlesItems', () async {
+      const id = 1;
 
-//       // Verify that the result matches the expected puzzleItems
-//       expect(await result.first, puzzleItems);
-//     });
-//   });
-// }
+      await puzzlesItemDao.insertPuzzleItem(puzzleItem);
+
+      final result = puzzlesItemDao.streamAllPuzzlesItems(id);
+
+      // Verify that the streamAllPuzzlesItems method is called with the correct
+      // argument
+
+      // Verify that the result matches the expected puzzleItems
+      expect(result, emits(puzzleItems));
+    });
+  });
+}
