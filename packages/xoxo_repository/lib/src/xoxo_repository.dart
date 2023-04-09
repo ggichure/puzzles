142ffc12bc287b456b4d565c2d687fd09bdfc3b8
Griffins Gichure <g.gichure@outlook.com>
2023-04-09 17:04:22 +0300

    init xoxo repository

---
+++ packages/xoxo_repository/lib/src/xoxo_repository.dart
@@ -0,0 +1,7 @@
+/// {@template xoxo_repository}
+/// A Repository  that handles xoxo  generation  related functions.
+/// {@endtemplate}
+class XoxoRepository {
+  /// {@macro xoxo_repository}
+  const XoxoRepository();
+}
---
+++ packages/xoxo_repository/test/src/xoxo_repository_test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:test/test.dart';
+import 'package:xoxo_repository/xoxo_repository.dart';
+
+void main() {
+  group('XoxoRepository', () {
+    test('can be instantiated', () {
+      expect(XoxoRepository(), isNotNull);
+    });
+  });
+}
