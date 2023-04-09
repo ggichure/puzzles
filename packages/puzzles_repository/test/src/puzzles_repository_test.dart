af5dce1badff579e09b0e68a27d3a915357369c7
Griffins Gichure <g.gichure@outlook.com>
2023-04-09 12:45:57 +0300

    init puzzles repository

---
+++ packages/puzzles_repository/test/src/puzzles_repository_test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:puzzles_repository/puzzles_repository.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('PuzzlesRepository', () {
+    test('can be instantiated', () {
+      expect(PuzzlesRepository(), isNotNull);
+    });
+  });
+}
