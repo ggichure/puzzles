fe1a4cef5750ed0f95b772d1ae1d64ef3ede6561
Griffins Gichure <g.gichure@outlook.com>
2023-04-09 17:01:32 +0300

    xoxo generator and tests

--- packages/xoxo_generator/test/src/xoxo_generator_test.dart
+++ packages/xoxo_generator/test/src/xoxo_generator_test.dart
@@ -7,5 +7,27 @@ void main() {
     test('can be instantiated', () {
       expect(XoxoGenerator(), isNotNull);
     });
+
+    test('Can generate random xoxo and match iven length', () async {
+      final xoxoGenerator = XoxoGenerator();
+      // Test case 1: Generate word with length 7
+      final word1 = await xoxoGenerator.randomXOXOGenerator(7);
+      expect(word1, isA<List<String>>());
+      expect(word1.first.length, 7);
+      expect(word1[1].length, 7);
+    });
+
+    test('xoxo Generated xoxo that match length', () async {
+      final xoxoGenerator = XoxoGenerator();
+      // Test case 1: Generate word with length 7
+      final word1 = xoxoGenerator.generateMixedXOXO(7);
+      expect(word1, isA<String>());
+      expect(word1.length, 7);
+
+      // Test case 2: Generate word with length 7
+      final word2 = xoxoGenerator.generateSimilarXOXO(word1);
+      expect(word2, isA<String>());
+      expect(word2.length, 7);
+    });
   });
 }
