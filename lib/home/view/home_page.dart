3fac774e960f9a4633b79aa149a28c59a8aef6c9
Griffins Gichure <g.gichure@outlook.com>
2023-04-10 19:54:11 +0300

    run dart format

--- lib/home/view/home_page.dart
+++ lib/home/view/home_page.dart
@@ -12,42 +12,43 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     final l10n = context.l10n;
     return Scaffold(
-        body: CustomScrollView(
-      slivers: [
-        SliverAppBar(
-          title: Text(l10n.puzzlesAppBarTitle),
-          elevation: 0,
-        ),
-        SliverGrid.count(
-          crossAxisCount: 2,
-          childAspectRatio: 1.7,
-          children: [
-            PuzzleTypeCard(
-              asset: Assets.svgs.letters,
-              title: l10n.lettersText,
-              puzzletype: PUZZLETYPE.letters,
-            ),
-            PuzzleTypeCard(
-              asset: Assets.svgs.xoxo,
-              title: l10n.xoxoText,
-              puzzletype: PUZZLETYPE.xoxo,
-            ),
-            PuzzleTypeCard(
-              asset: Assets.svgs.shapes,
-              title: l10n.shapesText,
-              puzzletype: PUZZLETYPE.shapes,
-            ),
-            PuzzleTypeCard(
-              asset: Assets.svgs.mixed,
-              title: l10n.mixedPuzzlesText,
-              puzzletype: PUZZLETYPE.mixed,
-            ),
-          ],
-        ),
-        const SliverToBoxAdapter(
-          child: SizedBox(height: 120),
-        )
-      ],
-    ));
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            title: Text(l10n.puzzlesAppBarTitle),
+            elevation: 0,
+          ),
+          SliverGrid.count(
+            crossAxisCount: 2,
+            childAspectRatio: 1.7,
+            children: [
+              PuzzleTypeCard(
+                asset: Assets.svgs.letters,
+                title: l10n.lettersText,
+                puzzletype: PUZZLETYPE.letters,
+              ),
+              PuzzleTypeCard(
+                asset: Assets.svgs.xoxo,
+                title: l10n.xoxoText,
+                puzzletype: PUZZLETYPE.xoxo,
+              ),
+              PuzzleTypeCard(
+                asset: Assets.svgs.shapes,
+                title: l10n.shapesText,
+                puzzletype: PUZZLETYPE.shapes,
+              ),
+              PuzzleTypeCard(
+                asset: Assets.svgs.mixed,
+                title: l10n.mixedPuzzlesText,
+                puzzletype: PUZZLETYPE.mixed,
+              ),
+            ],
+          ),
+          const SliverToBoxAdapter(
+            child: SizedBox(height: 120),
+          )
+        ],
+      ),
+    );
   }
 }
